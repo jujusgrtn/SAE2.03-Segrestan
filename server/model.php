48a28ed33594c27f7dc97639566c1f90772f64eb
Segrestan Justine <segrestan1@mmi-dev.unilim.fr>
2026-04-22 15:11:17 +0200

    Test Clé SSh

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "segrestan1");
+define("DBLOGIN", "segrestan1");
+define("DBPWD", "segrestan1");
 
 
 function getAllMovies(){
